b73881c3a05d4c40e846901e797a0e1486141186
ILia Berestov <berestov_2000@bk.ru>
2021-06-11 15:08:29 +0300

    Test4 done

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,9 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
@@ -11,19 +12,25 @@ public class CashbackHackServiceTest {
     public void ShouldReturnResidue() {
         int actual = cashbackHackService.remain(900);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
 
     @Test
     public void AmountZero(){
         int actual = cashbackHackService.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
     @Test
     public void AmountMoreThenBoundary(){
         int actual = cashbackHackService.remain(1100);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void Amount1000(){
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(expected,actual);
     }
 }
